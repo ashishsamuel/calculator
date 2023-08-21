5e8fd1e55582517708070c595316214c2dd99107
ashish <ashishsamuelthomson@gmail.com>
2023-08-21 12:27:41 +0530

    Calculator

---
+++ main.js
@@ -0,0 +1,21 @@
+
+function display(content){
+    result.value += content;
+}
+
+function resetResult(){
+    result.value = "";
+}
+
+function exprEval(){
+    try{
+        result.value = eval(result.value);
+    }
+    catch{
+        result.value = "Invalid Expression";
+    }
+}
+
+function removeLast(){
+    result.value = result.value.slice(0,-1);
+}
